0d3586ae8facbad9236271f57c451833a36b5dd7
Davis Mednis <davis.mednis@esynergy.lv>
2016-08-05 11:07:24 +0300

    Raise payload limits

--- api/app.js
+++ api/app.js
@@ -17,8 +17,8 @@ app.environment = process.env.NODE_ENV || app.config.env || 'production';
 app.debug = app.config.debug || app.environment == 'development';
 var port = process.env.PORT || app.config.port || 4100;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '10mb'}));
 app.use(cookieParser());
 
 // Bootstrap DB models
