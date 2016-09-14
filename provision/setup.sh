ffbf4100f7dff375e230e5506aa1cb603e1d2e7e
Ainārs Sprosts <ainars.sprosts@esynergy.lv>
2016-09-14 14:31:28 +0300

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -8,7 +8,7 @@ echo "----------Installing packages----------"
 #repositories
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' \
     && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 apt-get update
 
